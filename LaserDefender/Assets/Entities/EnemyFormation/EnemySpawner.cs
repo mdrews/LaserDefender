147d77ee6fb2f33e5c4bef8ab5424ddfe1434e95
mdrews <matthewdrews@gmail.com>
2018-01-22 21:08:13 -0500

    Initial Commit

---
+++ LaserDefender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    private float speed = 5f;
+    public float width = 10f;
+    public float height = 5f;
+
+    public bool goRight = true;
+
+    public float padding = 1f;
+    private float xmin = 0;
+    private float xmax = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+        float distance = transform.position.z - Camera.main.transform.position.z;
+
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
+
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+    }
+	
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 change;
+        if((transform.position.x + .5*width) > xmax)
+        {
+            goRight = false;
+        }
+        else if ((transform.position.x - .5*width) < xmin)
+        {
+            goRight = true;
+        }
+        if (goRight == true)
+        {
+            change = Vector3.right;
+        }
+        else
+        {
+            change = Vector3.left;
+        }
+
+        transform.position += new Vector3(change.x * speed * Time.deltaTime, 0);
+	}
+}
---
+++ LaserDefender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 playerMove;
+    public float speed = 15f;
+    public float padding = 1f;
+    public GameObject PlayerLaserPrefab;
+    public float projectileSpeed;
+
+    public float xmin = 0;
+    public float xmax = 0;
+
+	// Use this for initialization
+	void Start () {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
+    }
+
+    void Fire()
+    {
+        GameObject beam = Instantiate(PlayerLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", .5f, .2f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
+
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, 0);
+
+	}
+}
